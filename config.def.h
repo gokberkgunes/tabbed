705bfdc2e567ceda33b9e4a0c28d39d604851a53
Gokberk Gunes <prhi2@tutanota.com>
2023-02-25 20:46:07 +0300

    changing colors and binds

--- config.def.h
+++ config.def.h
@@ -1,17 +1,15 @@
-/* See LICENSE file for copyright and license details. */
-
 /* appearance */
-static const char font[]        = "monospace:size=9";
+static const char font[]        = "Gohu GohuFont:size=11";
 static const char* normbgcolor  = "#222222";
 static const char* normfgcolor  = "#cccccc";
-static const char* selbgcolor   = "#555555";
-static const char* selfgcolor   = "#ffffff";
+static const char* selbgcolor   = "#b2f66f";
+static const char* selfgcolor   = "#222222";
 static const char* urgbgcolor   = "#111111";
 static const char* urgfgcolor   = "#cc0000";
 static const char before[]      = "<";
 static const char after[]       = ">";
 static const char titletrim[]   = "...";
-static const int  tabwidth      = 200;
+static const int  tabwidth      = 50;
 static const Bool foreground    = True;
 static       Bool urgentswitch  = False;
 
@@ -25,42 +23,43 @@ static Bool npisrelative  = False;
 
 #define SETPROP(p) { \
         .v = (char *[]){ "/bin/sh", "-c", \
-                "prop=\"`xwininfo -children -id $1 | grep '^     0x' |" \
+                "prop=\"$(xwininfo -children -id $1 | grep '^     0x' |" \
                 "sed -e's@^ *\\(0x[0-9a-f]*\\) \"\\([^\"]*\\)\".*@\\1 \\2@' |" \
-                "xargs -0 printf %b | dmenu -l 10 -w $1`\" &&" \
+                "tail -n +2 | dmenu -i -l 10 -p 'Switch to: ')\" &&" \
                 "xprop -id $1 -f $0 8s -set $0 \"$prop\"", \
                 p, winid, NULL \
         } \
 }
 
-#define MODKEY ControlMask
-static const Key keys[] = {
-	/* modifier             key        function     argument */
-	{ MODKEY|ShiftMask,     XK_Return, focusonce,   { 0 } },
-	{ MODKEY|ShiftMask,     XK_Return, spawn,       { 0 } },
+#define MOD Mod4Mask
+#define SHIFT ShiftMask
+#define CTRL ControlMask
+#define ALT Mod1Mask
+static Key keys[] = {
+	/* modifier	key        function     argument */
+	{ CTRL|SHIFT,   XK_Return, focusonce,   { 0 } },
+	{ CTRL|SHIFT,   XK_Return, spawn,       { 0 } },
+	{ CTRL|SHIFT,   XK_l,      rotate,      { .i = +1 } },
+	{ CTRL|SHIFT,   XK_h,      rotate,      { .i = -1 } },
+	{ CTRL|SHIFT,   XK_j,      movetab,     { .i = -1 } },
+	{ CTRL|SHIFT,   XK_k,      movetab,     { .i = +1 } },
+	{ CTRL,         XK_Tab,    rotate,      { .i = 0 } },
+	{ CTRL,         XK_grave,  spawn,       SETPROP("_TABBED_SELECT_TAB") },
+	{ CTRL,         XK_1,      move,        { .i = 0 } },
+	{ CTRL,         XK_2,      move,        { .i = 1 } },
+	{ CTRL,         XK_3,      move,        { .i = 2 } },
+	{ CTRL,         XK_4,      move,        { .i = 3 } },
+	{ CTRL,         XK_5,      move,        { .i = 4 } },
+	{ CTRL,         XK_6,      move,        { .i = 5 } },
+	{ CTRL,         XK_7,      move,        { .i = 6 } },
+	{ CTRL,         XK_8,      move,        { .i = 7 } },
+	{ CTRL,         XK_9,      move,        { .i = 8 } },
+	{ CTRL,         XK_0,      move,        { .i = 9 } },
 
-	{ MODKEY|ShiftMask,     XK_l,      rotate,      { .i = +1 } },
-	{ MODKEY|ShiftMask,     XK_h,      rotate,      { .i = -1 } },
-	{ MODKEY|ShiftMask,     XK_j,      movetab,     { .i = -1 } },
-	{ MODKEY|ShiftMask,     XK_k,      movetab,     { .i = +1 } },
-	{ MODKEY,               XK_Tab,    rotate,      { .i = 0 } },
+	{ CTRL,         XK_q,      killclient,  { 0 } },
 
-	{ MODKEY,               XK_grave,  spawn,       SETPROP("_TABBED_SELECT_TAB") },
-	{ MODKEY,               XK_1,      move,        { .i = 0 } },
-	{ MODKEY,               XK_2,      move,        { .i = 1 } },
-	{ MODKEY,               XK_3,      move,        { .i = 2 } },
-	{ MODKEY,               XK_4,      move,        { .i = 3 } },
-	{ MODKEY,               XK_5,      move,        { .i = 4 } },
-	{ MODKEY,               XK_6,      move,        { .i = 5 } },
-	{ MODKEY,               XK_7,      move,        { .i = 6 } },
-	{ MODKEY,               XK_8,      move,        { .i = 7 } },
-	{ MODKEY,               XK_9,      move,        { .i = 8 } },
-	{ MODKEY,               XK_0,      move,        { .i = 9 } },
-
-	{ MODKEY,               XK_q,      killclient,  { 0 } },
-
-	{ MODKEY,               XK_u,      focusurgent, { 0 } },
-	{ MODKEY|ShiftMask,     XK_u,      toggle,      { .v = (void*) &urgentswitch } },
-
-	{ 0,                    XK_F11,    fullscreen,  { 0 } },
+	{ CTRL|SHIFT,   XK_u,      focusurgent, { 0 } },
+	/* { CTRL,         XK_u,      toggle,      { .v = (void*) &urgentswitch } },*/
+	{ 0,         XK_F11,       fullscreen, { 0 } },
 };
+
